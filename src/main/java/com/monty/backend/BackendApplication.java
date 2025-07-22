0dd6f934e30a8ab775e643d0fb77a8512b8d1ca3
Ray Atanas <r.atanas@bcomsat.com>
2025-07-22 21:05:04 +0300

    Added component scan

--- src/main/java/com/monty/backend/BackendApplication.java
+++ src/main/java/com/monty/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package com.monty.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@ComponentScan(basePackages = "com.monty.backend")
 public class BackendApplication {
 
 	public static void main(String[] args) {
